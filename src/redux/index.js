6bf36d514c9dbb4502d222f0fd013493d8f0cc7c
shawon39 <shshawon39@gmail.com>
2020-11-05 15:52:13 +0600

    async and redux thunk and axios

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,3 +1,4 @@
-export { buyCake } from './cake/cakeActions';
-export { buyIceCream } from './iceCream/iceCreamActions';
-export { buyChocolate } from './chocolate/chocolateActions';
+export { buyCake } from "./cake/cakeActions";
+export { buyIceCream } from "./iceCream/iceCreamActions";
+export { buyChocolate } from "./chocolate/chocolateActions";
+export * from "./user/userActions";
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,12 @@
 import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "./rootReducer";
-import logger from 'redux-logger';
-import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)));
+const store = createStore(
+	rootReducer,
+	composeWithDevTools(applyMiddleware(logger, thunk ))
+);
 
 export default store;
- 
